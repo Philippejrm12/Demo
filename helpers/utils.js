555dc826c36ad85181b74e123ad0d1b5513e35e0
Bamatembera, Christian <christian.bamatembera@bellmedia.ca>
2017-10-02 15:37:55 -0400

    Lowering prices to attract our customers

--- helpers/utils.js
+++ helpers/utils.js
@@ -29,15 +29,15 @@ module.exports = {
 
   loadData: function() {
     products = [
-      { id: 1, name: 'Plane tickets', description: 'Trip to south africa', price: '1000', image: 'http://lorempixel.com/400/200/transport/1/' },
-      { id: 2, name: 'Plane tickets', description: 'Small Plane trip', price: '999', image: 'http://lorempixel.com/400/200/transport/2/' },
-      { id: 3, name: 'Car test drive', description: '2 laps in a lamborghini', price: '800', image: 'http://lorempixel.com/400/200/transport/3/' },
-      { id: 4, name: 'Bus tour', description: 'Tour of london', price: '10000', image: 'http://lorempixel.com/400/200/transport/4/' },
-      { id: 5, name: 'Porsche cayman', description: '1 Tire', price: '2000', image: 'http://lorempixel.com/400/200/transport/5/' },
-      { id: 6, name: 'Train', description: 'Trip to Toronto', price: '300', image: 'http://lorempixel.com/400/200/transport/6/' },
-      { id: 7, name: 'Car', description: 'Mini cooper', price: '5000', image: 'http://lorempixel.com/400/200/transport/8/' },
-      { id: 8, name: 'Train', description: 'Trip to Quebec', price: '120' , image: 'http://lorempixel.com/400/200/transport/9/' },
-      { id: 9, name: 'Motocycle', description: '2 laps on a motocycle', price: '222', image: 'http://lorempixel.com/400/200/transport/10/' },
+      { id: 1, name: 'Plane tickets', description: 'Trip to south africa', price: '100', image: 'http://lorempixel.com/400/200/transport/1/' },
+      { id: 2, name: 'Plane tickets', description: 'Small Plane trip', price: '99', image: 'http://lorempixel.com/400/200/transport/2/' },
+      { id: 3, name: 'Car test drive', description: '2 laps in a lamborghini', price: '80', image: 'http://lorempixel.com/400/200/transport/3/' },
+      { id: 4, name: 'Bus tour', description: 'Tour of london', price: '1000', image: 'http://lorempixel.com/400/200/transport/4/' },
+      { id: 5, name: 'Porsche cayman', description: '1 Tire', price: '200', image: 'http://lorempixel.com/400/200/transport/5/' },
+      { id: 6, name: 'Train', description: 'Trip to Toronto', price: '30', image: 'http://lorempixel.com/400/200/transport/6/' },
+      { id: 7, name: 'Car', description: 'Mini cooper', price: '500', image: 'http://lorempixel.com/400/200/transport/8/' },
+      { id: 8, name: 'Train', description: 'Trip to Quebec', price: '12' , image: 'http://lorempixel.com/400/200/transport/9/' },
+      { id: 9, name: 'Motocycle', description: '2 laps on a motocycle', price: '22', image: 'http://lorempixel.com/400/200/transport/10/' },
     ];
     storage.init()
     .then(function() {
